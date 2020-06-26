188e2deddd999b99c6dd393fab8fd59bdc2409da
David-Stoner <david.aaron.stoner@gmail.com>
2020-06-26 17:53:37 +0000

    edits to comply with code review

--- lesson_2/mortgage_calculator/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -14,15 +14,12 @@ end
 
 def float?(input)
   if input.to_f.to_s == input
-    float = true
+    true
   elsif input.to_f.to_s == (input + '0')
-    float = true
-  elsif ('0' + input).to_f.to_s == ('0' + input)
-    float =  true
+    true
   else
-    float = false
+    ('0' + input).to_f.to_s == ('0' + input)
   end
-  float
 end
 
 def valid_number?(input)
@@ -38,52 +35,56 @@ def prompt(key, language)
   puts "=> #{message}"
 end
 
-def pick_language
+def display_pick_language
   prompt('language_select', 'en')
 end
 
-def welcome(language)
+def display_welcome(language)
   prompt('welcome', language)
 end
 
-def invalid_name(language)
+def display_invalid_name(language)
   prompt('invalid_name', language)
 end
 
-def get_loan(language)
+def display_get_loan(language)
   prompt('enter_loan', language)
 end
 
-def get_duration(language)
+def display_get_duration(language)
   prompt('enter_duration', language)
 end
 
-def get_rate(language)
+def display_get_rate(language)
   prompt('enter_rate', language)
   prompt('example', language)
 end
 
-def invalid_number(language)
+def display_invalid_number(language)
   prompt('invalid_number', language)
 end
 
-def output(language, monthly_payment)
+def display_invalid_integer(language)
+  prompt('invalid_whole_number', language)
+end
+
+def display_output(language, monthly_payment)
   puts format(messages('result', language), pmnt: monthly_payment.round(2).to_s)
 end
 
-def again(language)
+def display_again(language)
   prompt('again', language)
 end
 
-def goodbye(language, user_name)
+def display_goodbye(language, user_name)
   puts format(messages('goodbye', language), name: user_name)
 end
 
-def greeting(language, user_name)
+def display_greeting(language, user_name)
   puts format(messages('greeting', language), name: user_name)
 end
 
-def instructions(language)
+def display_instructions(language)
   prompt('instructions', language)
 end
 
@@ -107,23 +108,23 @@ def find_amortization(amort_denom, monthly_rate)
   (monthly_rate - 1) / amort_denom
 end
 
-def find_monthly_payment(loan_amount, amortization)
+def calculate_monthly_payment(loan_amount, amortization)
   loan_amount.to_i * amortization
 end
 
-def do_the_math(loan_amount, loan_duration, annual_rate)
+def find_monthly_paymnt(loan_amount, loan_duration, annual_rate)
   monthly_duration = find_monthly_duration(loan_duration)
   monthly_rate = find_monthly_rate(annual_rate)
   denom_monthlies = find_monthly_denom_factors(monthly_rate, monthly_duration)
   amort_denom = find_amortization_denominator(denom_monthlies)
   amortization = find_amortization(amort_denom, monthly_rate)
-  find_monthly_payment(loan_amount, amortization)
+  calculate_monthly_payment(loan_amount, amortization)
 end
 
 def select_language
   language_prompt = ''
   loop do
-    pick_language
+    display_pick_language
     language_prompt = gets.chomp
     break if (language_prompt == '1') || (language_prompt == '2')
   end
@@ -133,12 +134,12 @@ end
 
 def get_name(language)
   loop do
-    welcome(language)
+    display_welcome(language)
     user_name = gets.chomp
     if user_name.empty?
-      invalid_name(language)
+      display_invalid_name(language)
     elsif /\A\s*\z/.match(user_name)
-      invalid_name(language)
+      display_invalid_name(language)
     else
       return user_name
     end
@@ -147,43 +148,43 @@ end
 
 def get_loan_amount(language)
   loop do
-    get_loan(language)
+    display_get_loan(language)
     loan_amount = gets.chomp
     if valid_number?(loan_amount)
       return loan_amount
     else
-      invalid_number(language)
+      display_invalid_number(language)
     end
   end
 end
 
 def get_loan_duration(language)
   loop do
-    get_duration(language)
+    display_get_duration(language)
     loan_duration = gets.chomp
     if integer?(loan_duration) && loan_duration.to_i > 0
       return loan_duration
     else
-      invalid_number(language)
+      display_invalid_integer(language)
     end
   end
 end
 
 def get_loan_rate(language)
   loop do
-    get_rate(language)
+    display_get_rate(language)
     annual_rate = gets.chomp
     if valid_number?(annual_rate)
       return annual_rate
     else
-      invalid_number(language)
+      display_invalid_number(language)
     end
   end
 end
 
 def another_time(language)
   loop do
-    again(language)
+    display_again(language)
     go_again = gets.chomp
     if (go_again.downcase == 'y') || (go_again.downcase == 's')
       return true
@@ -196,16 +197,16 @@ end
 system('clear') || system('cls')
 language = select_language
 user_name = get_name(language)
-greeting(language, user_name)
-instructions(language)
+display_greeting(language, user_name)
+display_instructions(language)
 loop do
   loan_amount = get_loan_amount(language)
   loan_duration = get_loan_duration(language)
   annual_rate = get_loan_rate(language)
-  monthly_payment = do_the_math(loan_amount, loan_duration, annual_rate)
-  output(language, monthly_payment)
+  monthly_payment = find_monthly_paymnt(loan_amount, loan_duration, annual_rate)
+  display_output(language, monthly_payment)
   break unless another_time(language) == true
   system('clear') || system('cls')
 end
 
-goodbye(language, user_name)
+display_goodbye(language, user_name)
