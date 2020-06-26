d732f14de0f8e1a8b0d816d00845eef64811423d
David-Stoner <david.aaron.stoner@gmail.com>
2020-06-26 06:37:56 +0000

    refactored calculator to use yaml and pull out functions

--- lesson_2/calculator/calculator.rb
+++ lesson_2/calculator/calculator.rb
@@ -1,32 +1,20 @@
+require 'yaml'
+MESSAGES = YAML.load_file('messages.yml')
+
 number1 = ''
 number2 = ''
 name = ''
 operation = ''
-language = 'english.txt'
+language = 'en'
 language_prompt = ''
 
-welcome = 1
-name_get = 2
-hi = 3
-get_number1 = 4
-invalid_number = 5
-get_number2 = 6
-invalid_choice = 7
-calculating = 8
-display_result = 9
-again = 10
-goodbye = 11
-operator_choice = 12
-
-def prompt(message)
-  puts "=> #{message}"
+def messages(message, language)
+  MESSAGES[language][message]
 end
 
-def get_line(filename, lineno)
-  File.open(filename, 'r') do |getline|
-    (lineno - 1).times { getline.gets }
-    getline.gets
-  end
+def prompt(key, language)
+  message = messages(key, language)
+  puts "=> #{message}"
 end
 
 def integer?(input)
@@ -34,7 +22,12 @@ def integer?(input)
 end
 
 def float?(input)
-  input.to_f.to_s == input || input.to_f.to_s == (input + '0')
+  case input
+  when input.to_f.to_s == input then true
+  when input.to_f.to_s == (input + '0') then true
+  when ('0' + input).to_f.to_s == ('0' + input) then true
+  else false
+  end
 end
 
 def valid_number?(input)
@@ -54,86 +47,162 @@ def op_to_msg(operation)
   end
 end
 
-def invalid_number_display(language, invalid_number)
-  prompt get_line(language, invalid_number)
+def invalid_number_display(language)
+  prompt('invalid_number', language)
 end
 
-loop do
-  prompt "1) for english, \n  2) para español"
-  language_prompt = gets.chomp
-  break if (language_prompt == '1') || (language_prompt == '2')
+def language_select
+  prompt('language_select', 'en')
 end
 
-language = 'english.txt' if language_prompt == '1'
-language = 'spanish.txt' if language_prompt == '2'
+def welcome(language)
+  prompt('welcome', language)
+end
 
-loop do
-  prompt get_line(language, welcome)
-  name = gets.chomp
+def name_get(language)
+  prompt('name_get', language)
+end
 
-  if name.empty?
-    prompt get_line(language, name_get)
-  else
-    break
+def hi(language, name)
+  puts format(messages('hi', language), name: name)
+end
+
+def instructions(language)
+  prompt('instructions', language)
+end
+
+def number1_prompt(language)
+  prompt('get_number1', language)
+end
+
+def number2_prompt(language)
+  prompt('get_number2', language)
+end
+
+def operator_choice(language)
+  puts format(messages('operator_choice', language), line: "\n")
+end
+
+def invalid_choice(language)
+  prompt('invalid_choice', language)
+end
+
+def calculating_message(language, operation)
+  puts format(messages('calculating', language), operation: op_to_msg(operation))
+end
+
+def display_result(language, result)
+  puts format(messages('display_result', language), result: result)
+end
+
+def again_message(language)
+  prompt('again', language)
+end
+
+def goodbye(language, name)
+  puts format(messages('goodbye', language), name: name)
+end
+
+def pick_language(language_prompt, language)
+  loop do
+    language_select
+    language_prompt = gets.chomp
+    break if (language_prompt == '1') || (language_prompt == '2')
+  end
+  return 'en' if language_prompt == '1'
+  return 'es' if language_prompt == '2'
+end
+
+def enter_name(language)
+  loop do
+    welcome(language)
+    name = gets.chomp
+    if name.empty?
+      name_get(language)
+    else
+      return name
+    end
   end
 end
 
-prompt get_line(language, hi) % { name: name }
-
-loop do
+def get_number1(language)
   loop do
-    prompt get_line(language, get_number1)
+    number1_prompt(language)
     number1 = gets.chomp
 
     if valid_number?(number1)
-      break
+      return number1
     else
-      invalid_number_display(language, invalid_number)
+      invalid_number_display(language)
     end
   end
+end
 
+def get_number2(language)
   loop do
-    prompt get_line(language, get_number2)
+    number2_prompt(language)
     number2 = gets.chomp
 
     if valid_number?(number2)
-      break
+      return number2
     else
-      invalid_number_display(language, invalid_number)
+      invalid_number_display(language)
     end
   end
+end
 
-  prompt get_line(language, operator_choice) % { line: "\n" }
-
+def get_operation(language)
   loop do
     operation = gets.chomp
 
     if %w(1 2 3 4).include?(operation)
-      break
+      return operation
     else
-      prompt get_line(language, invalid_choice)
+      invalid_choice(language)
     end
   end
+end
 
-  prompt get_line(language, calculating) % { operation: op_to_msg(operation) }
+def get_result(number1, number2, operation)
+  case operation
+  when '1'
+    number1.to_i + number2.to_i
+  when '2'
+    number1.to_i - number2.to_i
+  when '3'
+    number1.to_i * number2.to_i
+  when '4'
+    number1.to_f / number2.to_f
+  end
+end
 
-  result = case operation
-           when '1'
-             number1.to_i + number2.to_i
-           when '2'
-             number1.to_i - number2.to_i
-           when '3'
-             number1.to_i * number2.to_i
-           when '4'
-             number1.to_f / number2.to_f
-           end
+def another_time(language)
+  loop do
+    again_message(language)
+    go_again = gets.chomp
+    if (go_again.downcase == 'y') || (go_again.downcase == 's')
+      return true
+    elsif (go_again.downcase == 'n')
+      return false
+    end
+  end
+end
 
-  prompt get_line(language, display_result) % { result: result }
-
-  prompt get_line(language, again)
-  go_again = gets.chomp
-  break unless (go_again.downcase == 'y') || (go_again.downcase == 's')
+system('clear') || system('cls')
+language = pick_language(language_prompt, language)
+name = enter_name(language)
+hi(language, name)
+instructions(language)
+loop do
+  number1 = get_number1(language)
+  number2 = get_number2(language)
+  operator_choice(language)
+  operation = get_operation(language)
+  calculating_message(language, operation)
+  result = get_result(number1, number2, operation)
+  display_result(language, result)
+  break unless another_time(language) == true
   system('clear') || system('cls')
 end
 
-prompt get_line(language, goodbye) % { name: name }
+goodbye(language, name)
